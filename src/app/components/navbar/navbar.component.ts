1eed5a42cc725abe8bf6fc80a52e0e0cf2f670b5
efren.delosreyes@origon.io <efren.delosreyes@origon.io>
2024-09-18 14:07:09 -0600

    Changes in navbar draft

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -6,5 +6,22 @@ import { Component } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
+  isDropdownOpenProducts = false;
+  isDropdownOpenResources = false;
 
+  openDropdownResources() {
+    this.isDropdownOpenResources = true;
+  }
+
+  closeDropdownResources() {
+    this.isDropdownOpenResources = false;
+  }
+
+  openDropdownProducts() {
+    this.isDropdownOpenProducts = true;
+  }
+
+  closeDropdownProducts() {
+    this.isDropdownOpenProducts = false;
+  }
 }
